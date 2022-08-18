c59baf3940117a2065c70bd401f2ace72d7c0638
Mariana Machohan <debrisofme9@gmail.com>
2022-08-18 05:53:32 +0200

    +props

--- src/App.js
+++ src/App.js
@@ -10,16 +10,32 @@ import Error from './Error';
 
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 
+const navProps = {
+  nav: [
+    { "link": "/", "text": "Главная" },
+    { "link": "/about", "text": "О сайте" },
+    { "link": "/cat", "text": "Категории" },
+  ]
+}
+
+const catProps = {
+	nav: [
+		{ "link": "/notebook", "text": "Ноутбуки" },
+		{ "link": "/monitor", "text": "Мониторы" },
+		{ "link": "/cellphone", "text": "Мобильные телефоны" }
+	]
+
+}
+
 function App() {
   return (
 		<>
 			<Router>
-				<Header />
-
+				<Header data={navProps} />
 				<Routes>
 					<Route exact path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />					
-					<Route exact path="/cat" element={<Category />} />
+					<Route exact path="/cat" element={<Category data={catProps} />} />
 					<Route path="cat/:catName" element={<CategoryDescription />} />
 					<Route path="*" element={< Error/>} />
 				</Routes>
