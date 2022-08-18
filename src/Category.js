87c5b2c0604da8aa82bc5774e5f2b10d1b522e19
Mariana Machohan <debrisofme9@gmail.com>
2022-08-18 05:53:35 +0200

    +props

--- src/Category.js
+++ src/Category.js
@@ -1,21 +1,21 @@
-import { useLocation } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
-function Category() {
+function Category(props) {
 	let url = useLocation();
-	console.log(url);
-	let {catName} = useParams();
-	console.log({catName});
+	let data = props.data.nav;
+	const listItem = data.map(item => <li key={item.link}><NavLink to={`${url.pathname}${item.link}`}>{item.text}</NavLink></li>);
 
 	return (
 		<>
 
 			<h1>Category</h1>
 			<ul>
-				<li><NavLink to={`${url.pathname}/notebook`}>Ноутбуки</NavLink></li>
+				{listItem}
+				{/* <li><NavLink to={`${url.pathname}/notebook`}>Ноутбуки</NavLink></li>
 				<li><NavLink to={`${url.pathname}/monitor`}>Мониторы</NavLink></li>
-				<li><NavLink to={`${url.pathname}/cellphone`}>Мобильные телефоны</NavLink></li>
+				<li><NavLink to={`${url.pathname}/cellphone`}>Мобильные телефоны</NavLink></li> */}
 			</ul>
 		</>
 	);
