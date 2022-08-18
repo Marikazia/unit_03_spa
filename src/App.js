34deddafd5a63bc007d995b0f8637fbfa685f332
Mariana Machohan <debrisofme9@gmail.com>
2022-08-18 05:21:37 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Category from './Category';
+import CategoryDescription from "./CategoryDescription";
+import Footer from './Footer';
+import Error from './Error';
+
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<>
+			<Router>
+				<Header />
+
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />					
+					<Route exact path="/cat" element={<Category />} />
+					<Route path="cat/:catName" element={<CategoryDescription />} />
+					<Route path="*" element={< Error/>} />
+				</Routes>
+
+				<Footer />
+			</Router>
+		</>
   );
 }
 
