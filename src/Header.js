554d92e463022da1711f504181114f69d2051d86
Mariana Machohan <debrisofme9@gmail.com>
2022-08-18 05:53:39 +0200

    +props

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom";
 
 
-function Header() {
+function Header(props) {
+	let data = props.data.nav;
+	const listItem = data.map(item => <li key={item.link}><NavLink to={item.link}>{item.text}</NavLink></li>);
   return (
 		<>
 			<ul>
-				<li><NavLink to="/">Главная</NavLink></li>
-				<li><NavLink to="/about">O сайте</NavLink></li>
-				<li><NavLink to="/cat">Категории</NavLink></li>
+				{listItem}
 			</ul>
 		</>
   );
